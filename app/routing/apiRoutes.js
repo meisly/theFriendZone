4fb89a2cc6c816fa7c673037ddb392a8df6c1f48
Tierra <sr.undershaft@gmail.com>
2019-09-18 14:04:01 -0500

    added summary, updated about me html and restyled modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
     let users = [{
         "name": "Ahmed",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "summary": "i can write",
         "scores": [
             5,
             1,
